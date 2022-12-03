1849a4d26808b414063ec79f5b565b85553a8e3d
Thogan24 <thogan1924@gmail.com>
2022-12-03 11:31:54 -0500

    Completed problem 12

---
+++ problem12.java
@@ -0,0 +1,28 @@
+public class problem12 {
+    public static void main(String[] args) {
+        int i = 1;
+        int triangleNumber = 0;
+        while(true){
+            triangleNumber = 0;
+            for(int j = 0; j < i; j++){
+                triangleNumber += j;
+            }
+            
+            if(numOfDivisors(triangleNumber) >= 300){
+                System.out.println(triangleNumber + " " + numOfDivisors(triangleNumber));
+                
+            }
+            i++;
+        }
+    }
+
+    public static int numOfDivisors(int n){
+        int count = 0;
+        for(int i = 1; i <= n; i++){
+            if(n % i == 0){
+                count++;
+            }
+        }
+        return count;
+    }
+}
