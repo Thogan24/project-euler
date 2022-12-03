e029e61c9cd66924d07dc988de6fc737b5a1067b
Thogan24 <thogan1924@gmail.com>
2022-12-03 09:10:29 -0500

    Solved problem 6

---
+++ problem6.java
@@ -0,0 +1,23 @@
+import java.util.concurrent.SubmissionPublisher;
+
+public class problem6 {
+    public static void main(String[] args) {
+        System.out.printf("%.0f", squareOfSums(100) - sumOfSquares(100));
+    }
+
+    public static double squareOfSums(int numSquare){
+        double sumSquare = 0;
+        for(int i = 1; i <= numSquare; i++){
+            sumSquare += i;
+        }
+        return Math.pow(sumSquare, 2);
+    }
+
+    public static double sumOfSquares(int numSum){
+        double sumSum = 0;
+        for(int j = 1; j <= numSum; j++){
+            sumSum += Math.pow(j, 2);
+        }
+        return sumSum;
+    }
+}
