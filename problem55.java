aca6169054da1a3897d0ef01788add414ae5bc87
Thogan24 <thogan1924@gmail.com>
2022-12-11 20:55:46 -0500

    Created isPalindromic method for problem55 will continue later

---
+++ problem55.java
@@ -0,0 +1,25 @@
+public class problem55 {
+    public static void main(String[] args) {
+        System.out.println("");
+    }
+
+    public static boolean isPalindromic(int num){
+        // store the number to originalNum
+        int originalNum = num;
+        int reversedNum = 0;
+        int remainder;
+        // get the reverse of originalNum
+        // store it in variable
+        while (num != 0) {
+          remainder = num % 10;
+          reversedNum = reversedNum * 10 + remainder;
+          num /= 10;
+        }
+    
+        // check if reversedNum and originalNum are equal
+        if (originalNum == reversedNum) {
+          return true;
+        }
+        return false;
+    }
+}
