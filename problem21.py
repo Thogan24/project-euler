57728c08c92efd56cbd08a31715da0d836b7ba98
Thogan24 <thogan1924@gmail.com>
2022-12-11 20:26:57 -0500

    Completed problem 23, attempted 21 in python

---
+++ problem21.py
@@ -0,0 +1,34 @@
+def main():
+    #Loop through all numbers till 10000
+    #Check if amicable
+    #Add all amicable numbers up till 10000
+    sum = 0
+    for i in range(9998):
+        if (isAmicable(i+2)):
+            sum += i
+        #print(sum)
+        
+
+def isAmicable(i):
+    sumi = sumOfDivisors(i)
+    for k in range (i):
+        if (sumi == sumOfDivisors(k)):
+            return True
+    for j in range (10000 - i):
+        if(sumi == sumOfDivisors(j+i)):
+            return True
+        
+    return False
+
+def sumOfDivisors(num):
+    sum = 0
+    for l in range(num-2):
+        print(l+1)
+        print(num-2)
+        if (num-2 % l+1 == 0):
+            sum += l+1
+
+    return sum
+
+
+main()
---
+++ problem23.java
@@ -0,0 +1,47 @@
+public class problem23 {
+    public static void main(String[] args) {
+        boolean shouldBreak = false;
+        int sum = 0;
+        for (int i = 0; i < 100; i++){ // For all numbers less than 28123
+
+            for (int j = 0; j < i; j++){
+                if (isAbundant(j)){
+                    //System.out.println(j);
+                    for (int k = 0; k < i; k++){
+                        if (k+j == i && k != j && isAbundant(k)){
+                                //System.out.println(i + " can be written");
+                                shouldBreak = true;
+                                break;
+                        }
+                    }
+                }
+                if (shouldBreak){
+                    break;
+                }
+            }
+            if (shouldBreak == false){
+                sum += i;
+                //System.out.println(i + " CANNOT BE WRITTEN!!!!!!!!! | " + sum);
+                
+            }
+            shouldBreak = false;
+            System.out.println("The current sum is: " + sum + " | Index at: " + i);
+
+        }
+    }
+
+
+    
+    public static boolean isAbundant(int num){
+        int sum = 0;
+        for(int i = 1; i < num; i++){
+            if(num % i == 0){
+                sum+= i;
+            }
+        }
+        if (sum > num){
+            return true;
+        }
+        return false;
+    }
+}
