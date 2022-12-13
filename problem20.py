c7509dff436b9e246cc2142b5bc6c85b9bbea376
Thogan24 <thogan1924@gmail.com>
2022-12-13 05:45:50 -0500

    Solved problem 20

---
+++ problem20.py
@@ -0,0 +1,25 @@
+import math
+def main():
+    factorial100 = list(str(math.factorial(100)))
+    print(factorial100)
+    print(sumOfDigits(factorial100))
+
+def factorial(num):
+    product = 1
+    for i in range (num-1):
+        product *= (i+1)
+    return product
+
+def sumOfDigits(num):
+    # numString = str(num)
+    # sum = 0
+    # for i in range (num):
+    #     print(i)
+    #     sum += int(numString[i])
+    # return sum
+    b = [int(x) for x in num]
+    return sum(b)
+
+
+
+main()
