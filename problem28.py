9ae3509fb6392c6deca0f13cf4cd3815f3559522
Thogan24 <thogan1924@gmail.com>
2022-12-13 20:55:57 -0500

    Solved problem 28 using python

---
+++ problem28.py
@@ -0,0 +1,21 @@
+def main():
+    sum = 0
+    i = 1
+    spiralOutwardsCounter = 2
+    spiralOutwardsCounterAdd = 1
+
+    while (i <= 1002001):
+        sum += i
+        print(i)
+        i += spiralOutwardsCounter
+        spiralOutwardsCounterAdd += 1    
+        if (spiralOutwardsCounterAdd > 4):
+            spiralOutwardsCounter += 2
+            spiralOutwardsCounterAdd = 1
+        
+    
+    print(sum)
+
+
+
+main()
