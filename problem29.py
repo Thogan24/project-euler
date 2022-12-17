8a485c5fd4aaea32ba8c06156c105a68ad16d017
Thogan24 <thogan1924@gmail.com>
2022-12-17 10:48:51 -0500

    Solved problem 29 using python

---
+++ problem29.py
@@ -0,0 +1,12 @@
+def main():
+    listOfNumbers = []
+    
+
+    for a in range(2, 101):
+        for b in range (2, 101):
+            if (listOfNumbers.count(a**b) == 0):
+                listOfNumbers.append(a**b)
+    
+    print(len(listOfNumbers))
+
+main()
