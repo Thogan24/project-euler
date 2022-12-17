71b4f12ca7b1bb3220e1bec70abcbc899c1e2254
Thogan24 <thogan1924@gmail.com>
2022-12-17 10:25:57 -0500

    Solved problem 30 using python

---
+++ problem30.py
@@ -0,0 +1,16 @@
+def main():
+    num = 2
+    sumOfAll = 0
+    while(True):
+        sumOfDigitsToPower = 0
+        b = [int(x) for x in str(num)]
+        for i in range(len(b)):
+            sumOfDigitsToPower += (b[i])**5
+        if(sumOfDigitsToPower == num):
+            sumOfAll += num
+        num += 1
+        print(sumOfAll)
+
+
+
+main()
