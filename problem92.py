cc16920245fec61163afe0834b7cc3d29ec2ffab
Thogan24 <thogan1924@gmail.com>
2022-12-17 19:27:00 -0500

    Solved problem 92 (answer takes very long though)

---
+++ problem92.py
@@ -0,0 +1,27 @@
+def main():
+    num = 1
+    count89 = 0
+    while(num < 10000000):
+        repeated = False
+        numList = []
+        i = num
+        while(repeated == False):
+            numList.append(i)
+            if(numList.count(89) > 0):
+                count89 += 1
+                repeated = True
+            elif(numList.count(1) > 0):
+                repeated = True
+            # elif (numList.count(i) > 1):
+            #     repeated = True
+            i = sumOfSquareOfDigits(i)
+        print(str(num) + " | " + str(count89))
+        num += 1
+
+            
+
+def sumOfSquareOfDigits(num):
+    b = [int(x)**2 for x in str(num)]
+    return sum(b)
+
+main()
