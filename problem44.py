17d33faf388b0a2df0f650f343f2ec40d141cd19
Thogan24 <thogan1924@gmail.com>
2022-12-20 19:16:38 -0500

    Updated problem 44

--- problem44.py
+++ problem44.py
@@ -1,19 +1,23 @@
 def main():
     pentagonalList = []
-    for k in range(100000):
+    length = 100000
+    for k in range(1, length):
         pentagonalList.append(pentagonal(k))
 
-    for i in range(100000):
-        for k in range (100000):
+    for i in range(length):
+        for j in range (length/10):
             pentagonali = pentagonalList[i]
             pentagonalj = pentagonalList[j]
-            if(pentagonalList.count(pentagonali + pentagonalj) >= 1 and pentagonalList.count(pentagonali - pentagonalj) >= 1):
-                print(str(pentagonali) + " | " + str(pentagonalj) + " | " + str(minimise()))
+            print(str(pentagonali) + " | " + str(pentagonalj))
+
+            if(pentagonalList.count(pentagonali + pentagonalj) >= 1 and pentagonalList.count(abs(pentagonali - pentagonalj)) >= 1):
+                print(str(pentagonali) + " | " + str(pentagonalj) + " | " + str(pentagonali - pentagonalj)) # Find smallest one of these
+                break
+            
+
 
 def pentagonal(num):
     return num * (3 * num - 1)/2
 
-def minimise(num1, num2):
-    # Finish minimise code
 
 main()
