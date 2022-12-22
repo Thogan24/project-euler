eda80e2ebad6efbe5bd20adb2470e055bda7fb90
Thogan24 <thogan1924@gmail.com>
2022-12-22 11:37:21 -0500

    Updated problem 43

--- problem43.py
+++ problem43.py
@@ -2,9 +2,21 @@
 
 def main():
     firstPrimeList = [2, 3, 5, 7, 11, 13, 17]
+    sumOfPandigitalProperties = 0
     for i in range(1000000000, 9999999999):
+        broke = False
         if(isPandigital(i)):
-            for i in range()
+            for j in range(7):
+                if(not (str(i[2+j]) + str(i[3+j]) + str(i[4+j]) % firstPrimeList[j] == 0)):
+                    broke = True
+                    break
+            if(not broke):
+                sumOfPandigitalProperties += i
+                print(sumOfPandigitalProperties)
+        
+    print(sumOfPandigitalProperties)
+                    
+
 
 def isPandigital(num):
     listWithAllDigits = []
