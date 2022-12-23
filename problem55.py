8177b2b50a41ed90bb204236c8ac34f886927e4d
Thogan24 <thogan1924@gmail.com>
2022-12-22 21:19:05 -0500

    Started problem 55 in python

---
+++ problem55.py
@@ -0,0 +1,22 @@
+# Unsolved
+
+def main():
+    for i in range(10000):
+
+
+def isPandigital(num):
+    listWithAllDigits = []
+    for i in range(1, len(str(num)) + 1):
+        listWithAllDigits += str(i)
+    
+    for j in range(len(str(num))):
+        if(listWithAllDigits.count(str(num)[j])):
+            listWithAllDigits.remove(str(num)[j])
+        else:
+            return False
+    if(len(listWithAllDigits) == 0):
+        return True
+
+    return False # Not sure if it should be able to get to this point or not
+
+main()
