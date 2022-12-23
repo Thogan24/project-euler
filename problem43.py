c19db8c068635cfff23157cf2a6d3ff3a9436bc3
Thogan24 <thogan1924@gmail.com>
2022-12-23 08:39:38 -0500

    Updated problem 43

--- problem43.py
+++ problem43.py
@@ -1,14 +1,16 @@
 # Unsolved
 
+# find a way for 000000000 to not go to 0
 def main():
     firstPrimeList = [2, 3, 5, 7, 11, 13, 17]
     sumOfPandigitalProperties = 0
-    for i in range(1406357289, 1406357289): #406357289 000000000 9999999999
+    for i in range(000000000, 9999999999): #406357289 000000000 9999999999
         broke = False
         if(isPandigital(i)):
             for j in range(7):
                 #print(float((str(i[1+j]) + str(i[2+j]) + str(i[3+j]))))
-                if(not (float((str(i[1+j]) + str(i[2+j]) + str(i[3+j]))) % firstPrimeList[j] == 0)):
+                numString = str(i)
+                if(not (int((str(numString[1+j]) + str(numString[2+j]) + str(numString[3+j]))) % int(firstPrimeList[j]) == 0)):
                     broke = True
                     break
             if(not broke):
@@ -17,7 +19,7 @@ def main():
         #print(i)
         
     #print(sumOfPandigitalProperties)
-    print(isPandigital(1406357289))
+    #print(isPandigital(1406357289))
                     
 
 # isPandigital working properly now
@@ -27,8 +29,8 @@ def isPandigital(num):
         listWithAllDigits += str(i)
     
     for j in range(len(str(num))):
-        print(listWithAllDigits)
-        print(listWithAllDigits.count(str(num)[j]))
+        #print(listWithAllDigits)
+        #print(listWithAllDigits.count(str(num)[j]))
         if(listWithAllDigits.count(str(num)[j]) == 1):
             listWithAllDigits.remove(str(num)[j])
         else:
