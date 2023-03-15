96f40c4115ec3cddf2620f2dee8636b24fdee4e8
Thogan24 <thogan1924@gmail.com>
2023-03-15 14:18:04 -0400

    Updated problem 44

--- problem44.py
+++ problem44.py
@@ -1,24 +1,28 @@
 def main():
     pentagonalList = []
-    length = 10000
+    length = 3000
+    smallestDifference = 1000000000
     for k in range(1, length+2):
+        print(pentagonal(k))
         pentagonalList.append(pentagonal(k))
 
     for i in range(length):
         for j in range (length):
             pentagonali = pentagonalList[i]
             pentagonalj = pentagonalList[j]
-            print(str(pentagonali) + " | " + str(pentagonalj) + " | ")
+            print(str(pentagonali) + " | " + str(pentagonalj) + " | " + str(smallestDifference))
 
             # This if statement doesn't work
-            if(pentagonalList.count(pentagonali + pentagonalj) >= 1 and pentagonalList.count(abs(pentagonali - pentagonalj)) >= 1):
+            if(pentagonalList.count((pentagonali + pentagonalj)) >= 1 and pentagonalList.count(abs((pentagonali - pentagonalj))) >= 1):
                 print(str(pentagonali) + " | " + str(pentagonalj) + " | " + str(pentagonali - pentagonalj)) # Find smallest one of these
-                break
+                if(abs(pentagonali - pentagonalj) < smallestDifference):
+                    smallestDifference = abs(pentagonali - pentagonalj)
+                
             
 
 
 def pentagonal(num):
-    return num * (3 * num - 1)/2
+    return (int) (num * (3 * num - 1)/2)
 
 
 main()
