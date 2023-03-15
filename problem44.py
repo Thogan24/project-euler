1a33b767fb9eb859358743243c8830b1e1d3ffa0
Thogan24 <thogan1924@gmail.com>
2023-03-15 11:37:02 -0400

    Changed up problem 44

--- problem44.py
+++ problem44.py
@@ -1,15 +1,16 @@
 def main():
     pentagonalList = []
-    length = 100000
-    for k in range(1, length):
+    length = 10000
+    for k in range(1, length+2):
         pentagonalList.append(pentagonal(k))
 
     for i in range(length):
-        for j in range (length/10):
+        for j in range (length):
             pentagonali = pentagonalList[i]
             pentagonalj = pentagonalList[j]
-            print(str(pentagonali) + " | " + str(pentagonalj))
+            print(str(pentagonali) + " | " + str(pentagonalj) + " | ")
 
+            # This if statement doesn't work
             if(pentagonalList.count(pentagonali + pentagonalj) >= 1 and pentagonalList.count(abs(pentagonali - pentagonalj)) >= 1):
                 print(str(pentagonali) + " | " + str(pentagonalj) + " | " + str(pentagonali - pentagonalj)) # Find smallest one of these
                 break
