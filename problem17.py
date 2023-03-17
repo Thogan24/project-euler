f513492cc02cca0eff7b89825eaabb4cdb8dee85
Thogan24 <thogan1924@gmail.com>
2023-03-17 14:20:26 -0400

    Started problem 17

--- problem17.py
+++ problem17.py
@@ -4,3 +4,54 @@
 # Create an array with all numbers 1-9, 10-90 (tens place), 100-900 (hundreds place) in words
 # Loop through all nums, add appropriate ands, tally, print
 
+num = 0
+
+while(num <= 1000):
+    letters = 0
+    numberSingleDigit = num % 10
+    numberDoubleDigit = (num % 100) / 10 
+    numberTripleDigit = num / 100
+    
+
+    match numberSingleDigit:
+        case 1:
+            letters += 3
+        case 2:
+            letters += 3
+        case 3:
+            letters += 5
+        case 4:
+            letters += 4
+        case 5:
+            letters += 4
+        case 6:
+            letters += 3
+        case 7:
+            letters += 5
+        case 8:
+            letters += 5
+        case 9:
+            letters += 4
+    
+    match numberDoubleDigit:
+        case 1:
+            letters += 
+        case 2:
+            letters += 6
+        case 3:
+            letters += 6
+        case 4:
+            letters += 6
+        case 5:
+            letters += 5
+        case 6:
+            letters += 5
+        case 7:
+            letters += 7
+        case 8:
+            letters += 6
+        case 9:
+            letters += 6
+        
+        
+        
---
+++ test.py
@@ -0,0 +1,1 @@
+print(len("one"))
